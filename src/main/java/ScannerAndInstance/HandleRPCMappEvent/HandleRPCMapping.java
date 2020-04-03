9e40645bfddfc494e8e5a3ed2ca6df3869f77d02
mcl <48353260+mcl973@users.noreply.github.com>
2020-04-03 19:55:22 +0800

    Update HandleRPCMapping.java

--- src/main/java/ScannerAndInstance/HandleRPCMappEvent/HandleRPCMapping.java
+++ src/main/java/ScannerAndInstance/HandleRPCMappEvent/HandleRPCMapping.java
@@ -42,44 +42,46 @@ public class HandleRPCMapping extends AbstractHandleRPCmapping {
             Method[] declaredMethods = aClass.getDeclaredMethods();
             for (Method declaredMethod : declaredMethods) {
                 if (declaredMethod.isAnnotationPresent(MethodRPC.class)) {
-                    //将这个函数的信息封装成protobuf类型的格式
-                    JieXiMethodRPC jieXiMethodRPC = new JieXiMethodRPC();
-                    //先获取方法上的名字
-                    String s = jieXiMethodRPC.JiexiAnnotation(declaredMethod);
-                    MethodInfos.ParagramesInfo.Builder builder = MethodInfos.ParagramesInfo.newBuilder();
-                    Parameter[] parameters = declaredMethod.getParameters();
-                    MethodInfos.ParagramesInfo build1 = null;
-                    if(parameters.length>0) {
-                        //获取参数
-                        for (Parameter parameter : parameters) {
-                            builder.setParagrameName(parameter.getName());
-                            builder.setParagrameType(parameter.getType().toString());
-                        }
-                        build1 = builder.build();
-                        //构建methidonfo
-                    }
-                    MethodInfos.MethodInfo build = null;
-                    if(build1!=null) {
-                        build = MethodInfos.MethodInfo.newBuilder()
-                                //到时可以使用这个快速的调取iocmap中具体的对象实例
-                                .setClassname(map.getKey())
-                                .setMethodname(s)
-                                .setMethodReturnType(declaredMethod.getReturnType().toString())
-                                .addParagrameinfo(build1).build();
-                    }else{
-                        build = MethodInfos.MethodInfo.newBuilder()
-                                //到时可以使用这个快速的调取iocmap中具体的对象实例
-                                .setClassname(map.getKey())
-                                .setMethodname(s)
-                                .setMethodReturnType(declaredMethod.getReturnType().toString()).build();
-                    }
-                    //装入map中
-                    method_Object.put(declaredMethod.getName(), build);
+                    getmethodinfo(map.getKey(),declaredMethod,method_Object);
                 }
             }
         }
         for(Map.Entry<String , MethodInfos.MethodInfo> map:method_Object.entrySet()){
             methodInfoMap.put(map.getKey(),map.getValue());
         }
+    }
+    public void getmethodinfo(String classname, Method declaredMethod, Map<String, MethodInfos.MethodInfo> method_Object){
+        JieXiMethodRPC jieXiMethodRPC = new JieXiMethodRPC();
+        //先获取方法上的名字
+        String s = jieXiMethodRPC.JiexiAnnotation(declaredMethod);
+        MethodInfos.ParagramesInfo.Builder builder = MethodInfos.ParagramesInfo.newBuilder();
+        Parameter[] parameters = declaredMethod.getParameters();
+        MethodInfos.ParagramesInfo build1 = null;
+        if(parameters.length>0) {
+            //获取参数
+            for (Parameter parameter : parameters) {
+                builder.setParagrameName(parameter.getName());
+                builder.setParagrameType(parameter.getType().toString());
+            }
+            build1 = builder.build();
+            //构建methidonfo
+        }
+        MethodInfos.MethodInfo build = null;
+        if(build1!=null) {
+            build = MethodInfos.MethodInfo.newBuilder()
+                    //到时可以使用这个快速的调取iocmap中具体的对象实例
+                    .setClassname(classname)
+                    .setMethodname(s)
+                    .setMethodReturnType(declaredMethod.getReturnType().toString())
+                    .addParagrameinfo(build1).build();
+        }else{
+            build = MethodInfos.MethodInfo.newBuilder()
+                    //到时可以使用这个快速的调取iocmap中具体的对象实例
+                    .setClassname(classname)
+                    .setMethodname(s)
+                    .setMethodReturnType(declaredMethod.getReturnType().toString()).build();
+        }
+        //装入map中
+        method_Object.put(declaredMethod.getName(), build);
     }
 }
